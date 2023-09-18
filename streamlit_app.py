6877582acb4d383283271f2dc040289715dec29e
kgsathisnowflake <144537035+kgsathisnowflake@users.noreply.github.com>
2023-09-17 21:50:07 -0500

    User input fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 #streamlit.text(fruityvice_response)
 
 #Section to display API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #streamlit.text(fruityvice_response.json())
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
