481d8f8b6cbbdcc7df01f442c9e6a7c4915e92e5
kgsathisnowflake <144537035+kgsathisnowflake@users.noreply.github.com>
2023-09-18 22:42:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ except URLError as e:
 #streamlit.stop()
 
 #Funtion to List 
-streamlit.header("Vioew Our Fruit List - Add your Favorites!")
+streamlit.header("View Our Fruit List - Add your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
