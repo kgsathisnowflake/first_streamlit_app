a78d944cd33c600874fb4e0804f87c2821209a82
kgsathisnowflake <144537035+kgsathisnowflake@users.noreply.github.com>
2023-09-18 21:54:33 -0500

    function

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-#streamlit.text(fruityvice_response)
+#Function
+def get_fruityvice_date(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 
 #Section to display API response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -40,10 +45,10 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get the information.")
   else:
-    streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #streamlit.write('The user entered ', fruit_choice)
+    back_from_function = get_fruityvice_date(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
+    
 except URLError as e:
     streamlit.error()
 
